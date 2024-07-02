b2f60bcc1d796c3fbdc09b4ae39fc4a9166356ca
pedro felipe <pedrofelipinho@gmail.com>
2024-07-02 10:36:50 -0300

    Criando um cliente HTTP que faz uma requisição get para pegar o JSON com as músicas

--- ScreenSound-04-API/Program.cs
+++ ScreenSound-04-API/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using (HttpClient client = new HttpClient())
+{
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    Console.WriteLine(resposta);
+}
