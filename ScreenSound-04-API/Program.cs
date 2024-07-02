d4d8843849818a24270c53695aa2c98a5738fba7
pedro felipe <pedrofelipinho@gmail.com>
2024-07-02 14:10:29 -0300

    Filtrando o JSON selecionando todos os gêneros de todas as músicas usando o LINQ

--- ScreenSound-04-API/Program.cs
+++ ScreenSound-04-API/Program.cs
@@ -1,9 +1,14 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreenSound_04_API.Filtros;
+using ScreenSound_04_API.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
     } catch (Exception ex)
     {
         Console.WriteLine($"Temos um problema: {ex.Message}");
