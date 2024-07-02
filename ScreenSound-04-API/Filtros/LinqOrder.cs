07af72d5adac474a3c18cec32e74369b2468e913
pedro felipe <pedrofelipinho@gmail.com>
2024-07-02 15:00:05 -0300

    Criando outra classe usando LINQ para filtrar e ordenar os artistas e exibindo no Program.cs

---
+++ ScreenSound-04-API/Filtros/LinqOrder.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound_04_API.Modelos;
+
+namespace ScreenSound_04_API.Filtros;
+
+internal class LinqOrder
+{
+    public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas)
+    {
+        var artistasOrdenados = musicas.OrderBy(musica => musica.Artista).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine("Lista de artistas ordenados");
+        foreach (var artista in artistasOrdenados)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+}
